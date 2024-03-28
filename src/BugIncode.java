ebe3e8e26bd9e6f6d3ff796a48ae1a755d9dd9f8
1neza <inezajean22@gmail.com>
2024-03-28 09:24:07 +0100

    Task 2 done...3

--- src/BugIncode.java
+++ src/BugIncode.java
@@ -18,21 +18,21 @@ import java.util.SortedSet;
     }
 }*/
 //Task 3
-public class BugIncode{
-    public static double Large_sib{
-
-        Scanner Scn = new Scanner(System.in);
-        System.out.print("\nEnter the non negative integer in range 0 to 10,000: ");
-        String len = Scn.nextLine();
-        String rev = "";
-
-        double num = Double.parseDouble(len);
-
-        for(int i = 1; i < len.length(); i++){
-            for(int j = 1; j < len.length() ; j--){
-                rev += len.charAt(j);
-            }
-        }
-
-    }
-}
+//public class BugIncode{
+//    public static double Large_sib{
+//
+//        Scanner Scn = new Scanner(System.in);
+//        System.out.print("\nEnter the non negative integer in range 0 to 10,000: ");
+//        String len = Scn.nextLine();
+//        String rev = "";
+//
+//        double num = Double.parseDouble(len);
+//
+//        for(int i = 1; i < len.length(); i++){
+//            for(int j = 1; j < len.length() ; j--){
+//                rev += len.charAt(j);
+//            }
+//        }
+//
+//    }
+//}
